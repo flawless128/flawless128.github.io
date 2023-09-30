d8c44e4f9dba2b2733c6c3dc283cbdf14b9c39e9
Garrett Haynes <flawless821@gmail.com>
2023-09-29 22:56:10 -0500

    Scroll as a function

--- _site/assets/javascript/script.js
+++ _site/assets/javascript/script.js
@@ -1,8 +1,8 @@
 // Used to calculate from slightly below the scroll positions
-// equal to 1/4 the height of the panel
+// equal to 1/4 the height of the panel (.66vh * .25 = .165vh)
 const panelOffset =  window.innerHeight *.165;
 // Offset distance from the top of the panel position
-// Two different ranges used to help prevent lots of CSS transitions triggering
+// Two different ranges used to help prevent lots of CSS transitions triggering at once
 // * 4 = height of a single panel
 const fadeEnterRange = panelOffset * 3.75;
 const fadeExitRange = panelOffset * 2.6;
@@ -10,23 +10,27 @@ const fadeExitRange = panelOffset * 2.6;
 var scrollFromTop, negations;
 
 $( document ).ready(function() {
-  $( document ).on("scroll", function() {
-    // Current scroll position
-    scrollFromTop = $( "html" ).scrollTop();
-    //console.log(scrollFromTop);
-
-    negations = scrollFromTop + panelOffset;
-
-    // When a panel is in view
-    $( ".gallery-thumbnail" ).filter(function (){
-      return (Math.abs( ( $(this).offset().top ) - negations) < fadeEnterRange ) ? true : false;
-    })
-      .css("filter", "saturate(100%)");
-
-    // When a panel is out of view
-    $( ".gallery-thumbnail" ).filter(function (){
-      return (Math.abs( ( $(this).offset().top ) - negations) >= fadeExitRange ) ? true : false;
-    })
-      .css("filter", "saturate(0%)");
-  });
+  // Fade panels on scroll and resize
+  $( document ).scroll(fadeScroll);
+  $( document ).resize(fadeScroll);
 });
+
+fadeScroll = function() {
+  // Current scroll position
+  scrollFromTop = $( "html" ).scrollTop();
+  //console.log(scrollFromTop);
+
+  negations = scrollFromTop + panelOffset;
+
+  // When a panel is in view
+  $( ".gallery-thumbnail" ).filter(function (){
+    return (Math.abs( ( $(this).offset().top ) - negations) < fadeEnterRange ) ? true : false;
+  })
+    .css("filter", "saturate(100%)");
+
+  // When a panel is out of view
+  $( ".gallery-thumbnail" ).filter(function (){
+    return (Math.abs( ( $(this).offset().top ) - negations) >= fadeExitRange ) ? true : false;
+  })
+    .css("filter", "saturate(0%)");
+}

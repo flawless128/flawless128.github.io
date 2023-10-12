49b8f1ebd6b2a0f63c35de663bc0f60f92354a43
Garrett Haynes <flawless821@gmail.com>
2023-10-11 22:03:36 -0500

    Use body overflow to prevent scrolling instead

--- _site/assets/javascript/script.js
+++ _site/assets/javascript/script.js
@@ -24,9 +24,6 @@ $( document ).ready(function() {
     $( ".custom2 > .custom2-text" ).toggleClass( "open", 180 );
   });
 
-  // Root of the page
-  const rootElement = $(':eq(0)');
-
   /*----------[ Image Viewer ]----------*/
   $( ".img-full-container" ).on( "click", function() {
     var fadeInTime = 280;
@@ -35,7 +32,7 @@ $( document ).ready(function() {
     // If the image IS NOT being viewed
     if (viewingImage === false) {
       // Prevent scrolling by "fixed-scroll" class to root element
-      rootElement.toggleClass("fixed-scroll");
+      $( "body" ).toggleClass("fixed-scroll");
       
       // Add markup to contain the loaded image
       $( ".featured-image" ).before( "<div class='ajax-image-container'><img class='ajax-image'></div>" );
@@ -62,7 +59,7 @@ $( document ).ready(function() {
     }
     else {
       // Allow scrolling again
-      rootElement.toggleClass("fixed-scroll");
+      $( "body" ).toggleClass("fixed-scroll");
       // If the image IS being viewed
       viewingImage = false;
       // Fade-out the image

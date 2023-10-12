46468364501918e2655161f7ebce525845ff3401
Garrett Haynes <flawless821@gmail.com>
2023-10-11 21:03:23 -0500

    Added interactive/animated image viewing

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,19 +8,13 @@ const fadeEnterRange = panelOffset * 3.75;
 const fadeExitRange = panelOffset * 2.6;
 
 var scrollFromTop, negations;
+// Whether an image is fullscreened or not
+var viewingImage = false;
 
 $( document ).ready(function() {
   // Fade panels on scroll and resize
   $( document ).scroll(fadeScroll);
   $( document ).resize(fadeScroll);
-
-  /*
-  $( ".custom" ).hover(function() {
-    $(this).animate({paddingLeft: '+=1.5rem'}, 250, 'swing');
-  }, function() {
-    $(this).animate({paddingLeft: '-=1.5rem'}, 250, 'swing');
-  });
-*/
 
   $( ".custom1" ).on( "click", function() {
     $( ".custom1 > .custom1-text" ).toggleClass( "open", 180 );
@@ -29,7 +23,70 @@ $( document ).ready(function() {
   $( ".custom2" ).on( "click", function() {
     $( ".custom2 > .custom2-text" ).toggleClass( "open", 180 );
   });
+
+  /*----------[ Image Viewer ]----------*/
+  $( ".img-full-container" ).on( "click", function() {
+    var fadeInTime = 280;
+    var fadeOutTime= 240;
+
+    // If the image IS NOT being viewed
+    if (viewingImage === false) {
+      // Add markup to contain the loaded image
+      $( ".featured-image" ).before( "<div class='ajax-image-container'><img class='ajax-image'></div>" );
+      // Fade-in the image
+      $( ".ajax-image" ).animate({
+        opacity: 1.0
+      }, fadeInTime, "swing");
+      // Fade-in container's opacity to 0.75
+      $( ".ajax-image-container" ).animate({
+        opacity: 0.75
+      }, fadeInTime, "swing");
+
+      /* [ Load in the image asynchronously ]
+        Images in the article have a high and low res version. The low res versions loaded with 
+        the article uses the "pre_" prefix in their filename. This Algorithm finds the current 
+        path name, then removes "pre_" from the string to get the path to the higher res version. */
+      // get image path
+      var imgElement = $(this).find( ".in-article" ).attr('src');
+      // remove pre_ from string
+      var newPath = imgElement.replace('pre_', '');
+      //console.log( newPath ); // log generated path string
+      loadImage( newPath );
+      viewingImage = true;
+    }
+    else {
+      // If the image IS being viewed
+      viewingImage = false;
+      // Fade-out the image
+      $( ".ajax-image" ).animate({
+        opacity: 0.0
+      }, fadeOutTime, "swing");
+      // Fade-out opacity, then remove the element
+      $( ".ajax-image-container" ).animate({
+        opacity: 0.0
+      }, fadeOutTime, "swing", function() {
+        $(this).remove();
+      });
+    }
+  });
 });
+
+
+/** Load an image asynchronously with jQuery's '.load'
+ *  Arguments: [path] - relative file location
+ */
+loadImage = function(path) {
+  var imgElement = $("<img>");
+  // Class storing the image
+  var image = $(".ajax-image");
+  // Asynchronously load
+  imgElement.on('load', function(){
+    image.attr("src", $(this).attr("src"));	
+  });
+  // Set the src attribute
+  imgElement.attr("src", path);
+}
+
 
 fadeScroll = function() {
   // Current scroll position
